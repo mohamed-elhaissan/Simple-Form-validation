266dce7ad4a4ddabfbace69a27d1a0d5eb0f1419
mohamed <mhalwasoffice@gmail.com>
2024-06-04 04:36:09 +0100

    errors

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const email = document.getElementById("email");
 const input = document.querySelectorAll("input");
 const label = document.querySelectorAll("label")
 
+
 for (let i = 0; i < input.length; i++) {
    input[i].addEventListener("focus",()=>{
     label[i].style.top = "-10px";
+    
    })
     
 }
@@ -17,3 +19,30 @@ textarea.addEventListener("focus",()=>{
     label[label.length-1].style.top = "-13px";
     textarea.style.opacity = ".4"
 })
+
+form.addEventListener("submit",(e)=>{
+    input.forEach(element => {
+        if (element.value == "" || element.value == null) {
+            e.preventDefault();
+            element.style.borderColor = "#e5383b";
+            let father = element.parentElement;
+            let icon = document.createElement("ion-icon");
+            icon.setAttribute("name","close-outline");
+            icon.classList.add("warning");
+            father.appendChild(icon);
+
+            element.addEventListener("input",()=>{
+                if(element.value !== ''){
+                    element.style.borderColor = "#55a630";
+                    let icon = element.parentElement.lastChild;
+                    icon.setAttribute("name","checkmark-outline");
+                                       
+                }
+            })
+            
+        }
+       
+        
+    });
+})
+a
